bed12ff65ff0206e67e6592b5a3e7593316cc3d3
GarrettGibbs <60447142+GarrettGibbs@users.noreply.github.com>
2020-02-07 12:12:32 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/JS/basic_javascript_2.js
@@ -0,0 +1,11 @@
+function function1() {
+    var str = "blue potato";
+    var result = str.fontcolor("blue");
+    document.getElementById("blue_text").innerHTML = result;
+}
+
+function function2() {
+    var newsent = "why would you do that?!?!";
+    newsent += " I had so much left to give!";
+    document.getElementById("Concatenate").innerHTML = newsent;
+}
