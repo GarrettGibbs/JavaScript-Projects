bfddbe2c863d3873428fd6780088cb4f30bf0103
GarrettGibbs <60447142+GarrettGibbs@users.noreply.github.com>
2020-02-10 10:33:00 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/JS/basic_javascript_5.js
@@ -0,0 +1,37 @@
+
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
+
+document.write(-2E310);
+
+document.write(2E310);
+
+document.write(100 >= 4)
+
+console.log(2 + 2);
+
+document.write("10" + 5);
+
+console.log(2>5);
+
+document.write(10==10);
+
+document.write(10==11);
+
+A = 10;
+B = 10;
+document.write(A===B);
+
+X = 10;
+Y = "10";
+document.write(X===Y);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
